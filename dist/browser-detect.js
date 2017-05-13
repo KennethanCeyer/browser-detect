a97ad284c02fdc5928274974d032b80513282413
KennethanCeyer <kennethan@nhpcw.com>
2017-05-14 03:03:38 +0900

    Overwrite issue are solved.

--- dist/browser-detect.js
+++ dist/browser-detect.js
@@ -463,62 +463,147 @@ var requirejs, _require, define;
 
 define("almond", function () {});
 
-if (typeof String.prototype.trim === 'undefined') {
-    String.prototype.trim = function () {
-        return String(this).replace(/^ +| +$/g, '');
-    };
-}
+define('polyfills', [], function () {
+    (function (method) {
+        var supportsAccessors = Object.prototype.hasOwnProperty('__defineGetter__');
+        var ERR_ACCESSORS_NOT_SUPPORTED = 'Getters & setters cannot be defined on this javascript engine';
+        var ERR_VALUE_ACCESSORS = 'A property cannot both have accessors and be writable or have a value';
 
-if (typeof Array.prototype.map === 'undefined') {
-    Array.prototype.map = function (fn) {
-        var rv = [];
-
-        for (var i = 0, l = this.length; i < l; i++) {
-            rv.push(fn(this[i]));
-        }return rv;
-    };
-}
-
-if (typeof Array.prototype.filter === 'undefined') {
-    Array.prototype.filter = function (fn) {
-        var rv = [];
-
-        for (var i = 0, l = this.length; i < l; i++) {
-            if (fn(this[i])) rv.push(this[i]);
-        }return rv;
-    };
-}
-
-if (typeof Object.prototype.assign === 'undefined') {
-    Object.prototype.assign = function (target, source) {
-        var from;
-        var to = !target ? Object(target) : {};
-        var symbols;
-
-        for (var s = 1; s < arguments.length; s++) {
-            from = Object(arguments[s]);
-
-            for (var key in from) {
-                if (Object.prototype.hasOwnProperty.call(from, key)) {
-                    to[key] = from[key];
-                }
+        Object.defineProperty = function defineProperty(object, property, descriptor) {
+            // Where native support exists, assume it
+            if (method && (object === window || object === document || object === Element.prototype || object instanceof Element)) {
+                return method(object, property, descriptor);
             }
 
-            if (Object.getOwnPropertySymbols) {
-                symbols = Object.getOwnPropertySymbols(from);
-                for (var i = 0; i < symbols.length; i++) {
-                    if (Object.prototype.propertyIsEnumerable.call(from, symbols[i])) {
-                        to[symbols[i]] = from[symbols[i]];
+            if (object === null || !(object instanceof Object || (typeof object === 'undefined' ? 'undefined' : _typeof(object)) === 'object')) {
+                throw new TypeError('Object.defineProperty called on non-object');
+            }
+
+            if (!(descriptor instanceof Object)) {
+                throw new TypeError('Property description must be an object');
+            }
+
+            var propertyString = String(property);
+            var hasValueOrWritable = 'value' in descriptor || 'writable' in descriptor;
+            var getterType = 'get' in descriptor && _typeof(descriptor.get);
+            var setterType = 'set' in descriptor && _typeof(descriptor.set);
+
+            // handle descriptor.get
+            if (getterType) {
+                if (getterType !== 'function') {
+                    throw new TypeError('Getter must be a function');
+                }
+                if (!supportsAccessors) {
+                    throw new TypeError(ERR_ACCESSORS_NOT_SUPPORTED);
+                }
+                if (hasValueOrWritable) {
+                    throw new TypeError(ERR_VALUE_ACCESSORS);
+                }
+                object.__defineGetter__(propertyString, descriptor.get);
+            } else {
+                object[propertyString] = descriptor.value;
+            }
+
+            // handle descriptor.set
+            if (setterType) {
+                if (setterType !== 'function') {
+                    throw new TypeError('Setter must be a function');
+                }
+                if (!supportsAccessors) {
+                    throw new TypeError(ERR_ACCESSORS_NOT_SUPPORTED);
+                }
+                if (hasValueOrWritable) {
+                    throw new TypeError(ERR_VALUE_ACCESSORS);
+                }
+                object.__defineSetter__(propertyString, descriptor.set);
+            }
+
+            // OK to define value unconditionally - if a getter has been specified as well, an error would be thrown above
+            if ('value' in descriptor) {
+                object[propertyString] = descriptor.value;
+            }
+
+            return object;
+        };
+    })(Object.defineProperty);
+
+    (function (method) {
+        if (typeof method === 'undefined') {
+            Object.defineProperty(Object, 'assign', {
+                enumerable: false,
+                configurable: true,
+                writable: true,
+                value: function (target, source) {
+                    var from;
+                    var to = target || {};
+                    var symbols;
+                    for (var s = 1; s < arguments.length; s++) {
+                        from = Object(arguments[s]);
+                        for (var key in from) {
+                            if (Object.prototype.hasOwnProperty.call(from, key)) {
+                                to[key] = from[key];
+                            }
+                        }
+                        if (Object.getOwnPropertySymbols) {
+                            symbols = Object.getOwnPropertySymbols(from);
+                            for (var i = 0; i < symbols.length; i++) {
+                                if (Object.prototype.propertyIsEnumerable.call(from, symbols[i])) {
+                                    to[symbols[i]] = from[symbols[i]];
+                                }
+                            }
+                        }
                     }
+                    return to;
                 }
-            }
+            });
         }
+    })(Object.prototype.assign);
 
-        return to;
-    };
-};
-define("polyfills", function () {});
+    (function (method) {
+        if (typeof method === 'undefined') {
+            Object.defineProperty(String, 'trim', {
+                enumerable: false,
+                configurable: true,
+                writable: true,
+                value: function () {
+                    return String(this).replace(/^ +| +$/g, '');
+                }
+            });
+        }
+    })(String.prototype.trim);
 
+    (function (method) {
+        if (typeof method === 'undefined') {
+            Object.defineProperty(Array, 'map', {
+                enumerable: false,
+                configurable: true,
+                writable: true,
+                value: function (fn) {
+                    var rv = [];
+                    for (var i = 0, l = this.length; i < l; i++) {
+                        rv.push(fn(this[i]));
+                    }return rv;
+                }
+            });
+        }
+    })(Array.prototype.map);
+
+    (function (method) {
+        if (typeof method === 'undefined') {
+            Object.defineProperty(Array, 'filter', {
+                enumerable: false,
+                configurable: true,
+                writable: true,
+                value: function (fn) {
+                    var rv = [];
+                    for (var i = 0, l = this.length; i < l; i++) {
+                        if (fn(this[i])) rv.push(this[i]);
+                    }return rv;
+                }
+            });
+        }
+    })(Array.prototype.filter);
+});
 var browsers = [['edge', /Edge\/([0-9\._]+)/], ['yandexbrowser', /YaBrowser\/([0-9\._]+)/], ['chrome', /(?!Chrom.*OPR)Chrom(?:e|ium)\/([0-9\.]+)(:?\s|$)/], ['crios', /CriOS\/([0-9\.]+)(:?\s|$)/], ['firefox', /Firefox\/([0-9\.]+)(?:\s|$)/], ['opera', /Opera\/([0-9\.]+)(?:\s|$)/], ['opera', /OPR\/([0-9\.]+)(:?\s|$)$/], ['ie', /Trident\/7\.0.*rv\:([0-9\.]+)\).*Gecko$/], ['ie', /MSIE\s([0-9\.]+);.*Trident\/[4-7].0/], ['ie', /MSIE\s(7\.0)/], ['bb10', /BB10;\sTouch.*Version\/([0-9\.]+)/], ['android', /Android\s([0-9\.]+)/], ['ios', /Version\/([0-9\._]+).*Mobile.*Safari.*/], ['safari', /Version\/([0-9\._]+).*Safari/]];
 
 var os = ['Windows Phone', 'Android', 'CentOS', { 'name': 'Chrome OS', 'pattern': 'CrOS' }, 'Debian', 'Fedora', 'FreeBSD', 'Gentoo', 'Haiku', 'Kubuntu', 'Linux Mint', 'OpenBSD', 'Red Hat', 'SuSE', 'Ubuntu', 'Xubuntu', 'Cygwin', 'Symbian OS', 'hpwOS', 'webOS ', 'webOS', 'Tablet OS', 'Tizen', 'Linux', 'Mac OS X', 'Macintosh', 'Mac', 'Windows 98;', 'Windows '];
--- src/polyfills.js
+++ src/polyfills.js
@@ -1,56 +1,141 @@
-if (typeof String.prototype.trim === 'undefined') {
-    String.prototype.trim = function() {
-        return String(this).replace(/^ +| +$/g, '')
-    };
-}
+define(() => {
+    (method => {
+        var supportsAccessors = Object.prototype.hasOwnProperty('__defineGetter__');
+        var ERR_ACCESSORS_NOT_SUPPORTED = 'Getters & setters cannot be defined on this javascript engine';
+        var ERR_VALUE_ACCESSORS = 'A property cannot both have accessors and be writable or have a value';
 
-if (typeof Array.prototype.map === 'undefined') {
-  Array.prototype.map = function(fn) {
-    var rv = [];
-    
-    for(var i=0, l=this.length; i<l; i++)
-      rv.push(fn(this[i]));
-
-    return rv;
-  };
-}
-
-if (typeof Array.prototype.filter === 'undefined') {
-  Array.prototype.filter = function(fn) {
-    var rv = [];
-    
-    for(var i=0, l=this.length; i<l; i++)
-      if (fn(this[i])) rv.push(this[i]);
-
-    return rv;
-  };
-}
-
-if (typeof Object.prototype.assign === 'undefined') {
-    Object.prototype.assign = function(target, source) {
-        var from;
-        var to = !target? Object(target) : {};
-        var symbols;
-
-        for (var s = 1; s < arguments.length; s++) {
-            from = Object(arguments[s]);
-
-            for (var key in from) {
-                if (Object.prototype.hasOwnProperty.call(from, key)) {
-                    to[key] = from[key];
-                }
+        Object.defineProperty = function defineProperty(object, property, descriptor) {
+            // Where native support exists, assume it
+            if (method && (object === window || object === document || object === Element.prototype || object instanceof Element)) {
+                return method(object, property, descriptor);
             }
 
-            if (Object.getOwnPropertySymbols) {
-                symbols = Object.getOwnPropertySymbols(from);
-                for (var i = 0; i < symbols.length; i++) {
-                    if (Object.prototype.propertyIsEnumerable.call(from, symbols[i])) {
-                        to[symbols[i]] = from[symbols[i]];
+            if (object === null || !(object instanceof Object || typeof object === 'object')) {
+                throw new TypeError('Object.defineProperty called on non-object');
+            }
+
+            if (!(descriptor instanceof Object)) {
+                throw new TypeError('Property description must be an object');
+            }
+
+            var propertyString = String(property);
+            var hasValueOrWritable = 'value' in descriptor || 'writable' in descriptor;
+            var getterType = 'get' in descriptor && typeof descriptor.get;
+            var setterType = 'set' in descriptor && typeof descriptor.set;
+
+            // handle descriptor.get
+            if (getterType) {
+                if (getterType !== 'function') {
+                    throw new TypeError('Getter must be a function');
+                }
+                if (!supportsAccessors) {
+                    throw new TypeError(ERR_ACCESSORS_NOT_SUPPORTED);
+                }
+                if (hasValueOrWritable) {
+                    throw new TypeError(ERR_VALUE_ACCESSORS);
+                }
+                object.__defineGetter__(propertyString, descriptor.get);
+            } else {
+                object[propertyString] = descriptor.value;
+            }
+
+            // handle descriptor.set
+            if (setterType) {
+                if (setterType !== 'function') {
+                    throw new TypeError('Setter must be a function');
+                }
+                if (!supportsAccessors) {
+                    throw new TypeError(ERR_ACCESSORS_NOT_SUPPORTED);
+                }
+                if (hasValueOrWritable) {
+                    throw new TypeError(ERR_VALUE_ACCESSORS);
+                }
+                object.__defineSetter__(propertyString, descriptor.set);
+            }
+
+            // OK to define value unconditionally - if a getter has been specified as well, an error would be thrown above
+            if ('value' in descriptor) {
+                object[propertyString] = descriptor.value;
+            }
+
+            return object;
+        };
+    })(Object.defineProperty);
+
+    (method => {
+        if (typeof method === 'undefined') {
+            Object.defineProperty(Object, 'assign', {
+                enumerable: false,
+                configurable: true,
+                writable: true,
+                value: function(target, source) {
+                    var from;
+                    var to = target || {};
+                    var symbols;
+                    for (var s = 1; s < arguments.length; s++) {
+                        from = Object(arguments[s]);
+                        for (var key in from) {
+                            if (Object.prototype.hasOwnProperty.call(from, key)) {
+                                to[key] = from[key];
+                            }
+                        }
+                        if (Object.getOwnPropertySymbols) {
+                            symbols = Object.getOwnPropertySymbols(from);
+                            for (var i = 0; i < symbols.length; i++) {
+                                if (Object.prototype.propertyIsEnumerable.call(from, symbols[i])) {
+                                    to[symbols[i]] = from[symbols[i]];
+                                }
+                            }
+                        }
                     }
+                    return to;
                 }
-            }
+            });
         }
+    })(Object.prototype.assign);
 
-        return to;
-    };
-}
+    (method => {
+        if (typeof method === 'undefined') {
+            Object.defineProperty(String, 'trim', {
+                enumerable: false,
+                configurable: true,
+                writable: true,
+                value: function() {
+                    return String(this).replace(/^ +| +$/g, '')
+                }
+            });
+        }
+    })(String.prototype.trim);
+
+    (method => {
+        if (typeof method === 'undefined') {
+            Object.defineProperty(Array, 'map', {
+                enumerable: false,
+                configurable: true,
+                writable: true,
+                value: function(fn) {
+                    var rv = [];                
+                    for(var i=0, l=this.length; i<l; i++)
+                    rv.push(fn(this[i]));
+                    return rv;
+                }
+            });
+        }
+    })(Array.prototype.map);
+
+    (method => {
+        if (typeof method === 'undefined') {
+            Object.defineProperty(Array, 'filter', {
+                enumerable: false,
+                configurable: true,
+                writable: true,
+                value: function(fn) {
+                    var rv = [];            
+                    for(var i=0, l=this.length; i<l; i++)
+                    if (fn(this[i])) rv.push(this[i]);
+                    return rv;
+                }
+            });
+        }
+    })(Array.prototype.filter);
+});
