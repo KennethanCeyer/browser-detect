0da1e608767c135a347279b3d1d1adc06b4fba5b
PIGNOSE <kennethan@nhpcw.com>
2018-04-01 10:16:25 +0900

    WIP: remove unnecessary lines

--- src/browser-detect.ts
+++ src/browser-detect.ts
@@ -4,10 +4,6 @@ import 'core-js/fn/array/filter';
 import 'core-js/fn/array/map';
 import 'core-js/fn/string/trim';
 
-const navigator = typeof window !== 'undefined'
-    ? window.navigator
-    : undefined;
-
 export default function (userAgent: string): BrowserDetectInfo {
     const detector = new Detector(userAgent, navigator, process);
     return detector.detect();
