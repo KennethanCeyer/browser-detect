a3524bec68291481b0bf65ab863a6c0c0f9e69fe
PIGNOSE <kennethan@nhpcw.com>
2018-04-02 21:36:56 +0900

    WIP: fix tsconfig.json file to create interface.ts properly

--- dist/browser-detect.es5.js
+++ dist/browser-detect.es5.js
@@ -41,7 +41,7 @@ var os = [
     'Windows Phone',
     'Android',
     'CentOS',
-    { 'name': 'Chrome OS', 'pattern': 'CrOS' },
+    { name: 'Chrome OS', pattern: 'CrOS' },
     'Debian',
     'Fedora',
     'FreeBSD',
@@ -82,6 +82,139 @@ var osVersions = {
     '4.0': 'NT',
     '4.90': 'ME'
 };
+
+var mobileRegExp = new RegExp(['(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|',
+    'compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|',
+    'midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)',
+    '\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|',
+    'wap|windows ce|xda|xiino'].join(''), 'i');
+var mobilePrefixRegExp = new RegExp(['1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\\-)|',
+    'ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\\-m|r |s )|',
+    'avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\\-(n|u)|c55\\/|capi|ccwa|cdm\\-|',
+    'cell|chtm|cldc|cmd\\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\\-s|devi|dica|dmob|do(c|p)o|',
+    'ds(12|\\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\\-|_)|',
+    'g1 u|g560|gene|gf\\-5|g\\-mo|go(\\.w|od)|gr(ad|un)|haie|hcit|hd\\-(m|p|t)|hei\\-|',
+    'hi(pt|ta)|hp( i|ip)|hs\\-c|ht(c(\\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\\-(20|go|ma)|',
+    'i230|iac( |\\-|\\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|',
+    'kddi|keji|kgt( |\\/)|klon|kpt |kwc\\-|kyo(c|k)|le(no|xi)|lg( g|\\/(k|l|u)|50|54|\\-[a-w])',
+    '|libw|lynx|m1\\-w|m3ga|m50\\/|ma(te|ui|xo)|mc(01|21|ca)|m\\-cr|me(rc|ri)|mi(o8|oa|ts)|',
+    'mmef|mo(01|02|bi|de|do|t(\\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|',
+    'n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|',
+    'op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\\-2|',
+    'po(ck|rt|se)|prox|psio|pt\\-g|qa\\-a|qc(07|12|21|32|60|\\-[2-7]|i\\-)|qtek|r380|r600|',
+    'raks|rim9|ro(ve|zo)|s55\\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\\-|oo|p\\-)|sdk\\/|',
+    'se(c(\\-|0|1)|47|mc|nd|ri)|sgh\\-|shar|sie(\\-|m)|k\\-0|sl(45|id)|sm(al|ar|b3|it|t5)|',
+    'so(ft|ny)|sp(01|h\\-|v\\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\\-|tdg\\-|',
+    'tel(i|m)|tim\\-|t\\-mo|to(pl|sh)|ts(70|m\\-|m3|m5)|tx\\-9|up(\\.b|g1|si)|utst|v400|v750|',
+    'veri|vi(rg|te)|vk(40|5[0-3]|\\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|',
+    'w3c(\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\\-|your|zeto|zte\\-'].join(''), 'i');
+
+var Detector = /** @class */ (function () {
+    function Detector(userAgent, navigator, process) {
+        this.navigator = navigator;
+        this.process = process;
+        this.userAgent = userAgent
+            ? userAgent
+            : this.navigator ? (navigator.userAgent || navigator.vendor) : '';
+    }
+    Detector.prototype.detect = function () {
+        if (this.process && !this.userAgent) {
+            var version = this.process.version.slice(1).split('.').slice(0, 3);
+            var versionTail = Array.prototype.slice.call(version, 1).join('') || '0';
+            return {
+                name: 'node',
+                version: version.join('.'),
+                versionNumber: parseFloat(version[0] + "." + versionTail),
+                mobile: false,
+                os: this.process.platform
+            };
+        }
+        if (!this.userAgent)
+            this.handleMissingError();
+        return __assign({}, this.checkBrowser(), this.checkMobile(), this.checkOs());
+    };
+    Detector.prototype.checkBrowser = function () {
+        var _this = this;
+        return browsers
+            .filter(function (definition) { return definition[1].test(_this.userAgent); })
+            .map(function (definition) {
+            var match = definition[1].exec(_this.userAgent);
+            var version = match && match[1].split(/[._]/).slice(0, 3);
+            var versionTails = Array.prototype.slice.call(version, 1).join('') || '0';
+            if (version && version.length < 3)
+                Array.prototype.push.apply(version, version.length === 1 ? [0, 0] : [0]);
+            return {
+                name: String(definition[0]),
+                version: version.join('.'),
+                versionNumber: Number(version[0] + "." + versionTails)
+            };
+        })
+            .shift();
+    };
+    Detector.prototype.checkMobile = function () {
+        var agentPrefix = this.userAgent.substr(0, 4);
+        var mobile = mobileRegExp.test(this.userAgent) || mobilePrefixRegExp.test(agentPrefix);
+        return { mobile: mobile };
+    };
+    Detector.prototype.checkOs = function () {
+        var _this = this;
+        return os
+            .map(function (definition) {
+            var name = definition.name || definition;
+            var pattern = _this.getOsPattern(definition);
+            return {
+                name: name,
+                pattern: pattern,
+                value: RegExp("\\b" + pattern.replace(/([ -])(?!$)/g, '$1?') + "(?:x?[\\d._]+|[ \\w.]*)", 'i').exec(_this.userAgent)
+            };
+        })
+            .filter(function (definition) { return definition.value; })
+            .map(function (definition) {
+            var os$$1 = definition.value[0] || '';
+            var osSuffix;
+            if (definition.pattern &&
+                definition.name &&
+                /^Win/i.test(os$$1) &&
+                !/^Windows Phone /i.test(os$$1) &&
+                (osSuffix = osVersions[os$$1.replace(/[^\d.]/g, '')]))
+                os$$1 = "Windows " + osSuffix;
+            if (definition.pattern && definition.name)
+                os$$1 = os$$1.replace(RegExp(definition.pattern, 'i'), definition.name);
+            os$$1 = os$$1
+                .replace(/ ce$/i, ' CE')
+                .replace(/\bhpw/i, 'web')
+                .replace(/\bMacintosh\b/, 'Mac OS')
+                .replace(/_PowerPC\b/i, ' OS')
+                .replace(/\b(OS X) [^ \d]+/i, '$1')
+                .replace(/\bMac (OS X)\b/, '$1')
+                .replace(/\/(\d)/, ' $1')
+                .replace(/_/g, '.')
+                .replace(/(?: BePC|[ .]*fc[ \d.]+)$/i, '')
+                .replace(/\bx86\.64\b/gi, 'x86_64')
+                .replace(/\b(Windows Phone) OS\b/, '$1')
+                .replace(/\b(Chrome OS \w+) [\d.]+\b/, '$1')
+                .split(' on ')[0]
+                .trim();
+            os$$1 = /^(?:webOS|i(?:OS|P))/.test(os$$1)
+                ? os$$1
+                : (os$$1.charAt(0).toUpperCase() + os$$1.slice(1));
+            return { os: os$$1 };
+        })
+            .shift();
+    };
+    Detector.prototype.getOsPattern = function (definition) {
+        var definitionInterface = definition;
+        return (typeof definition === 'string'
+            ? definition
+            : undefined) ||
+            definitionInterface.pattern ||
+            definitionInterface.name;
+    };
+    Detector.prototype.handleMissingError = function () {
+        throw new Error('Please give user-agent.\n> browser(navigator.userAgent or res.headers[\'user-agent\']).');
+    };
+    return Detector;
+}());
 
 function createCommonjsModule(fn, module) {
 	return module = { exports: {} }, fn(module, module.exports), module.exports;
@@ -489,131 +622,15 @@ _stringTrim('trim', function ($trim) {
 
 var trim$1 = _core.String.trim;
 
-var navigator = typeof window !== 'undefined'
+var injectableNavigator = typeof window !== 'undefined'
     ? window.navigator
     : undefined;
+var injectableProcess = typeof process !== 'undefined'
+    ? process
+    : undefined;
 function browserDetect (userAgent) {
-    var safeUserAgent = userAgent
-        ? userAgent
-        : navigator ? (navigator.userAgent || navigator.vendor) : '';
-    return runDetect(safeUserAgent);
-}
-function runDetect(userAgent) {
-    if (typeof process !== 'undefined' && !userAgent) {
-        var version = process.version
-            .slice(1).split('.').slice(0, 3);
-        var versionTail = Array.prototype.slice.call(version, 1).join('') || '0';
-        return {
-            name: 'node',
-            version: version.join('.'),
-            versionNumber: parseFloat(version[0] + "." + versionTail),
-            mobile: false,
-            os: process.platform
-        };
-    }
-    if (!userAgent) {
-        throw new Error('Please give user-agent.\n' +
-            '> browser(navigator.userAgent or res.headers[\'user-agent\']).');
-    }
-    return __assign({}, checkBrowser(userAgent), checkMobile(userAgent), checkOs(userAgent));
-}
-function checkBrowser(userAgent) {
-    return browsers
-        .filter(function (definition) { return definition[1].test(userAgent); })
-        .map(function (definition) {
-        var match = definition[1].exec(userAgent);
-        var version = match && match[1].split(/[._]/).slice(0, 3);
-        var versionTails = Array.prototype.slice.call(version, 1).join('') || '0';
-        if (version && version.length < 3)
-            Array.prototype.push.apply(version, version.length === 1 ? [0, 0] : [0]);
-        return {
-            name: String(definition[0]),
-            version: version.join('.'),
-            versionNumber: Number(version[0] + "." + versionTails)
-        };
-    })
-        .shift();
-}
-function checkMobile(userAgent) {
-    /* tslint:disable:align */
-    return {
-        mobile: (new RegExp('(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|' +
-            'compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|' +
-            'midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)' +
-            '\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|' +
-            'wap|windows ce|xda|xiino', 'i')
-            .test(userAgent) ||
-            new RegExp('1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\\-)|' +
-                'ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\\-m|r |s )|' +
-                'avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\\-(n|u)|c55\\/|capi|ccwa|cdm\\-|' +
-                'cell|chtm|cldc|cmd\\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\\-s|devi|dica|dmob|do(c|p)o|' +
-                'ds(12|\\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\\-|_)|' +
-                'g1 u|g560|gene|gf\\-5|g\\-mo|go(\\.w|od)|gr(ad|un)|haie|hcit|hd\\-(m|p|t)|hei\\-|' +
-                'hi(pt|ta)|hp( i|ip)|hs\\-c|ht(c(\\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\\-(20|go|ma)|' +
-                'i230|iac( |\\-|\\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|' +
-                'kddi|keji|kgt( |\\/)|klon|kpt |kwc\\-|kyo(c|k)|le(no|xi)|lg( g|\\/(k|l|u)|50|54|\\-[a-w])|' +
-                'libw|lynx|m1\\-w|m3ga|m50\\/|ma(te|ui|xo)|mc(01|21|ca)|m\\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|' +
-                'mo(01|02|bi|de|do|t(\\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|' +
-                'n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|' +
-                'owg1|p800|pan(a|d|t)|pdxg|pg(13|\\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\\-2|po(ck|rt|se)|prox|' +
-                'psio|pt\\-g|qa\\-a|qc(07|12|21|32|60|\\-[2-7]|i\\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\\/|' +
-                'sa(ge|ma|mm|ms|ny|va)|sc(01|h\\-|oo|p\\-)|sdk\\/|se(c(\\-|0|1)|47|mc|nd|ri)|sgh\\-|shar|sie(\\-|m)|' +
-                'k\\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\\-|v\\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|' +
-                'ta(gt|lk)|tcl\\-|tdg\\-|tel(i|m)|tim\\-|t\\-mo|to(pl|sh)|ts(70|m\\-|m3|m5)|tx\\-9|up(\\.b|g1|si)|' +
-                'utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|' +
-                'w3c(\\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\\-|your|zeto|zte\\-', 'i')
-                .test(userAgent.substr(0, 4)))
-    };
-    /* tslint:enable:align */
-}
-function checkOs(userAgent) {
-    return os
-        .map(function (definition) {
-        var name = definition.name || definition;
-        var pattern = (typeof definition === 'string'
-            ? definition
-            : undefined) ||
-            definition.pattern ||
-            name;
-        return {
-            name: name,
-            pattern: pattern,
-            value: RegExp("\\b" + pattern.replace(/([ -])(?!$)/g, '$1?') + "(?:x?[\\d._]+|[ \\w.]*)", 'i').exec(userAgent)
-        };
-    })
-        .filter(function (definition) { return definition.value; })
-        .map(function (definition) {
-        var os$$1 = definition.value[0] || '';
-        var osSuffix;
-        if (definition.pattern &&
-            definition.name &&
-            /^Win/i.test(os$$1) &&
-            !/^Windows Phone /i.test(os$$1) &&
-            (osSuffix = osVersions[os$$1.replace(/[^\d.]/g, '')]))
-            os$$1 = "Windows " + osSuffix;
-        if (definition.pattern && definition.name)
-            os$$1 = os$$1.replace(RegExp(definition.pattern, 'i'), definition.name);
-        os$$1 = os$$1
-            .replace(/ ce$/i, ' CE')
-            .replace(/\bhpw/i, 'web')
-            .replace(/\bMacintosh\b/, 'Mac OS')
-            .replace(/_PowerPC\b/i, ' OS')
-            .replace(/\b(OS X) [^ \d]+/i, '$1')
-            .replace(/\bMac (OS X)\b/, '$1')
-            .replace(/\/(\d)/, ' $1')
-            .replace(/_/g, '.')
-            .replace(/(?: BePC|[ .]*fc[ \d.]+)$/i, '')
-            .replace(/\bx86\.64\b/gi, 'x86_64')
-            .replace(/\b(Windows Phone) OS\b/, '$1')
-            .replace(/\b(Chrome OS \w+) [\d.]+\b/, '$1')
-            .split(' on ')[0]
-            .trim();
-        os$$1 = /^(?:webOS|i(?:OS|P))/.test(os$$1)
-            ? os$$1
-            : (os$$1.charAt(0).toUpperCase() + os$$1.slice(1));
-        return { os: os$$1 };
-    })
-        .shift();
+    var detector = new Detector(userAgent, injectableNavigator, injectableProcess);
+    return detector.detect();
 }
 
 export default browserDetect;
