0cffce5b07e9c3cb647be38600d71c2af728610c
Radu Dimitriade <radu.dimitriade@ownzones.com>
2018-06-06 16:03:11 +0300

    added ipad support

--- src/regexp.ts
+++ src/regexp.ts
@@ -1,7 +1,7 @@
 export const mobileRegExp =
     new RegExp(
         ['(android|bb\\d+|meego).+mobile|avantgo|bada\\/|blackberry|blazer|',
-        'compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|',
+        'compal|elaine|fennec|hiptop|iemobile|ip(hone|od|ad)|iris|kindle|lge |maemo|',
         'midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)',
         '\\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\\.(browser|link)|vodafone|',
         'wap|windows ce|xda|xiino'].join(''),
